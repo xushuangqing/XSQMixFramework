698e2bff2ce28e5b87a842b87d21f0a62ea9b624
xushuangqing <xushuangqing@bytedance.com>
2017-03-07 20:19:29 +0800

    Initial Commit

---
+++ XSQMixFramework/XSQMixFramework.h
@@ -0,0 +1,19 @@
+//
+//  XSQMixFramework.h
+//  XSQMixFramework
+//
+//  Created by 徐霜晴 on 17/3/7.
+//  Copyright © 2017年 XSQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XSQMixFramework.
+FOUNDATION_EXPORT double XSQMixFrameworkVersionNumber;
+
+//! Project version string for XSQMixFramework.
+FOUNDATION_EXPORT const unsigned char XSQMixFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XSQMixFramework/PublicHeader.h>
+
+
